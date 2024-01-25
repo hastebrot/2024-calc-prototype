54aac615b3fe273709e2d106cb07be398a3aab7b
Benjamin Gudehus <benjamin.gudehus@freiheit.com>
2024-01-25 09:08:44 +0100

    (wip)

--- calc-app/src/App.tsx
+++ calc-app/src/App.tsx
@@ -1,33 +1,9 @@
-import { derive } from "derive-valtio";
+// import { derive } from "derive-valtio";
 import * as Lucide from "lucide-react";
 import { nanoid } from "nanoid";
-import { Fragment, useCallback } from "react";
+import { Fragment } from "react";
 import { proxy, useSnapshot } from "valtio";
 import { Zod, z } from "./helper/zod";
-import { store } from "./store";
-
-type Item = {
-  id: string;
-  name: string;
-  price?: number;
-};
-
-const myProxy = proxy({
-  items: [] as Item[],
-});
-
-const fooProxy = proxy({
-  fooStore: store,
-  get fooCount() {
-    return this.fooStore.count + 1;
-  },
-});
-
-const barProxy = derive({
-  barCount(get) {
-    return get(store).count + 1;
-  },
-});
 
 const ItemSchema = z.object({
   id: z.string(),
@@ -54,45 +30,45 @@ const BoardSchema = z.object({
 
 type Board = z.infer<typeof BoardSchema>;
 
-const appState = proxy(
-  Zod.parse(BoardSchema, {
-    name: "Beispielkalkulation",
-    sections: [
-      {
-        id: nanoid(10),
-        name: "Gagen",
-        subsections: [
-          {
-            id: nanoid(10),
-            name: "Produktionsstab",
-            items: [
-              { id: nanoid(10), name: "Produzent", price: 1000 },
-              { id: nanoid(10), name: "Produktionsleitung", price: 900 },
-              { id: nanoid(10), name: "1. Aufnahmeleitung", price: 800 },
-            ],
-          },
-          {
-            id: nanoid(10),
-            name: "Regiestab",
-            items: [
-              { id: nanoid(10), name: "Regie", price: 1000 },
-              { id: nanoid(10), name: "1. Regieassistenz", price: 900 },
-              { id: nanoid(10), name: "Script / Continuity", price: 800 },
-            ],
-          },
-          {
-            id: nanoid(10),
-            name: "Kamerastab",
-            items: [
-              { id: nanoid(10), name: "Kamera", price: 1000 },
-              { id: nanoid(10), name: "1. Kameraassistenz", price: 900 },
-            ],
-          },
-        ],
-      },
-    ],
-  })
-);
+const exampleBoard = Zod.parse(BoardSchema, {
+  name: "Beispielkalkulation",
+  sections: [
+    {
+      id: nanoid(10),
+      name: "Gagen",
+      subsections: [
+        {
+          id: nanoid(10),
+          name: "Produktionsstab",
+          items: [
+            { id: nanoid(10), name: "Produzent", price: 1000 },
+            { id: nanoid(10), name: "Produktionsleitung", price: 900 },
+            { id: nanoid(10), name: "1. Aufnahmeleitung", price: 800 },
+          ],
+        },
+        {
+          id: nanoid(10),
+          name: "Regiestab",
+          items: [
+            { id: nanoid(10), name: "Regie", price: 1000 },
+            { id: nanoid(10), name: "1. Regieassistenz", price: 900 },
+            { id: nanoid(10), name: "Script / Continuity", price: 800 },
+          ],
+        },
+        {
+          id: nanoid(10),
+          name: "Kamerastab",
+          items: [
+            { id: nanoid(10), name: "Kamera", price: 1000 },
+            { id: nanoid(10), name: "1. Kameraassistenz", price: 900 },
+          ],
+        },
+      ],
+    },
+  ],
+});
+
+const appState = proxy(exampleBoard);
 
 const moneyFormat = new Intl.NumberFormat("en-US", {
   style: "currency",
@@ -134,11 +110,6 @@ export const App = () => {
             );
           })}
         </Board>
-      </div>
-
-      <div className="hidden">
-        <Bar />
-        <Foo />
       </div>
     </div>
   );
@@ -233,73 +204,6 @@ const Item = ({ children, ...props }: ItemProps) => {
       <div className="flex items-center gap-4">
         <div className="font-[600] text-sm tabular-nums">{moneyFormat.format(price)}</div>
         <Lucide.X size={18} />
-      </div>
-    </div>
-  );
-};
-
-const Bar = () => {
-  const { title } = useSnapshot(store, { sync: true });
-  const onChange = useCallback(
-    (event: React.ChangeEvent<HTMLInputElement>) => {
-      const value = event.currentTarget.value;
-      store.title = value;
-    },
-    [store]
-  );
-
-  return (
-    <div className="p-4">
-      <div>title: {title}</div>
-      <input className="block w-full hover:bg-gray-300" value={title} onChange={onChange} />
-    </div>
-  );
-};
-
-const Foo = () => {
-  const { title, count } = useSnapshot(store);
-  const foo = useSnapshot(fooProxy);
-  const bar = useSnapshot(barProxy);
-  const onClick = useCallback(() => {
-    store.title = "foobar";
-    store.count += 1;
-  }, [store]);
-  const onClick2 = useCallback(() => {
-    const item = { id: nanoid(10), name: "foo" };
-    myProxy.items.push(item);
-  }, [myProxy]);
-  const onClick3 = useCallback(
-    (item: Item) => {
-      const itemIndex = myProxy.items.findIndex((it) => it.id === item.id);
-      myProxy.items.splice(itemIndex, 1);
-    },
-    [myProxy]
-  );
-  const items = useSnapshot(myProxy.items);
-
-  return (
-    <div className="p-4">
-      <div>title: {title}</div>
-      <div>count: {count}</div>
-      <div>count: {foo.fooCount}</div>
-      <div>count: {bar.barCount}</div>
-      <button className="block hover:bg-gray-300" tabIndex={1} onClick={onClick}>
-        click
-      </button>
-      <button className="block hover:bg-gray-300" tabIndex={1} onClick={onClick2}>
-        click
-      </button>
-      <div>
-        {items.map((item) => {
-          return (
-            <div className="flex gap-2" key={item.id}>
-              <span>{item.id}</span>
-              <button className="block hover:bg-gray-300" onClick={() => onClick3(item)}>
-                remove
-              </button>
-            </div>
-          );
-        })}
       </div>
     </div>
   );
