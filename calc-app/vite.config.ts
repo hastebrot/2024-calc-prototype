56d58c9cd3a6d35e0edc34964850f2b4043ac52d
Benjamin Gudehus <benjamin.gudehus@freiheit.com>
2024-01-25 11:51:56 +0100

    chore: Manual chunks for react and react-router.

--- calc-app/vite.config.ts
+++ calc-app/vite.config.ts
@@ -3,4 +3,15 @@ import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ["react", "react-dom"],
+          "react-router": ["react-router", "react-router-dom"],
+        },
+      },
+    },
+  },
 });
