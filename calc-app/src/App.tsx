188e344232620dd5e8e172056bc78e45c60e97e7
Benjamin Gudehus <benjamin.gudehus@freiheit.com>
2024-01-25 06:03:24 +0100

    (wip)

--- calc-app/src/App.tsx
+++ calc-app/src/App.tsx
@@ -2,6 +2,13 @@ import { proxy, useSnapshot } from "valtio";
 import { derive } from "derive-valtio";
 import { store } from "./store";
 import { useCallback } from "react";
+import { nanoid } from "nanoid";
+
+type Item = { id: string; name: string };
+
+const myProxy = proxy({
+  items: [] as Item[],
+});
 
 const fooProxy = proxy({
   fooStore: store,
@@ -24,6 +31,14 @@ export const App = () => {
     store.title = "foobar";
     store.count += 1;
   }, [store]);
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.currentTarget.value;
+    store.title = value;
+  };
+  const onClick2 = useCallback(() => {
+    myProxy.items.push({ id: nanoid(10), name: "foo" });
+  }, [myProxy]);
+  const items = useSnapshot(myProxy.items);
 
   return (
     <div className="relative font-sans min-h-dvh">
@@ -32,9 +47,33 @@ export const App = () => {
         <div>count: {count}</div>
         <div>count: {foo.fooCount}</div>
         <div>count: {bar.barCount}</div>
-        <button tabIndex={1} onClick={onClick}>
+        <button className="block hover:bg-gray-300" tabIndex={1} onClick={onClick}>
           click
         </button>
+        <input className="block hover:bg-gray-300" value={title} onChange={onChange} />
+        <button className="block hover:bg-gray-300" tabIndex={1} onClick={onClick2}>
+          click
+        </button>
+        <div>
+          {items.map((item) => {
+            return (
+              <div className="flex" key={item.id}>
+                <span>{item.id}</span>
+                <button
+                  className="block hover:bg-gray-300"
+                  onClick={() => {
+                    myProxy.items.splice(
+                      myProxy.items.findIndex((it) => it.id === item.id),
+                      1
+                    );
+                  }}
+                >
+                  remove
+                </button>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
